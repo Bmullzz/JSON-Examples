fb53110841887c45cfd59b8d2ed9740291e6f418
Froilan Miranda <froilan.miranda@gmail.com>
2017-02-02 11:23:45 -0500

    update code samples

--- examples 4/index_alt.js
+++ examples 4/index_alt.js
@@ -2,22 +2,23 @@ var request = new XMLHttpRequest();
 var output;
 var data_json;
 
-request.open('GET', 'http://jsonplaceholder.typicode.com/posts/');
-//request.open('GET', 'https://raw.githubusercontent.com/mheadd/de-plow-history/master/data/196740.json');
+//request.open('GET', 'http://jsonplaceholder.typicode.com/posts/');
+request.open('GET', 'https://raw.githubusercontent.com/mheadd/de-plow-history/master/data/196740.json');
 
 request.onreadystatechange = function(){
 
 	if((request.status==200) && (request.readyState==4)){
 		data_json = JSON.parse(request.response);
+
 		for(var i in data_json){
 			var data = data_json[i];
 
+			console.log(data_json);
 			output = "<h3>" + data["title"] + "</h3>";
 			output += "<p>" + data["body"] + "</p>";
-
-			document.getElementById("feedContainer").innerHTML += output
-
 		}
+
+		document.getElementById("feedContainer").innerHTML += output
 
 	}
 }
